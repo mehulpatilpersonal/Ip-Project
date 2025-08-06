f2966ee82356c814e3720e8c0382696e9d608df2
mehulpatilpersonal <99871513+mehulpatilpersonal@users.noreply.github.com>
2025-08-07 01:22:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 #FIRST STEP OF CODE WILL BE WRITTEN HERE
 #This will be main file need to be Executed 
+
+
+def main_func(args,kwargs):
+  pass
+
+
+
+if __name__ == '__main__':
+  pass #running main function 
+  
