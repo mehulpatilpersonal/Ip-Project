85ddb32db9cdf6fef0a3eb0e62e0c31baf7f331a
Mehul Patil <mehulp214@gmail.com>
2025-08-08 00:51:59 +0530

    sdgsg

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+import platform
+import time
+
+def run_output_in_new_terminal():
+    script = 'output.py'
+    system = platform.system()
+
+    if system == 'Windows':
+        # Open new Command Prompt window and run output.py
+        subprocess.Popen(['start', 'cmd', '/k', f'python {script}'], shell=True)
+
+
+   
+if __name__ == '__main__':
+    run_output_in_new_terminal()
+    time.sleep(500)  # Optional: wait a bit before running the script
