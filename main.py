533df3924ee11420fde30d18a7b60a22a6922d8d
boomchikchik <bulkdeals99@gmail.com>
2025-08-08 22:25:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mysql-connector as myql
 # import subprocess
 # import platform
 # import time
@@ -10,7 +11,12 @@
 #         # Open new Command Prompt window and run output.py
 #         subprocess.Popen(['start', 'cmd', '/k', f'python {script}'], shell=True)
 
-
+def main_func():
+   while True:
+      Print("WELCOME TO VEHICLE MANAGEMENT SYSTEM")
+      choice=int(input("PLEASE SELECT FROM THE GIVEN OPTION \n\n 1. NEW USER \n\n 2.OLD USER \n\n 3. ADMIN \n\n 4. EXIT"))
+      
+      
    
 if __name__ == '__main__':
     run_output_in_new_terminal()
